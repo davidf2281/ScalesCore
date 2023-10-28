f0706411bfc1950b6f5fb421055374c1e9a5f20a
David Fearon <david@davidfearon.com>
2023-10-28 18:51:08 +0100

    Initial structure

--- Sources/ScalesCore/ReadingProvider.swift
+++ Sources/ScalesCore/ReadingProvider.swift
@@ -13,7 +13,7 @@ public protocol ReadingProviderDelegate {
     func didGetReading(_ reading: Float)
 }
 
-public protocol ReadingProvider {
+public protocol ReadingProvider: AnyObject {
     var delegate: ReadingProviderDelegate? { get set }
     func start()
 }
@@ -21,17 +21,20 @@ public protocol ReadingProvider {
 public protocol Display {
     init(width: CGFloat, height: CGFloat)
     var font: Font { get set }
-    func drawText(x: CGFloat, y: CGFloat)
+    func drawText(_ text: String, x: CGFloat, y: CGFloat)
 }
 
 public struct ReadingProcessor: ReadingProviderDelegate {
     let readingProvider: ReadingProvider
+    let display: Display
     
-    init(readingProvider: some ReadingProvider) {
+    public init(readingProvider: some ReadingProvider, display: Display) {
         self.readingProvider = readingProvider
+        self.display = display
+        self.readingProvider.delegate = self
     }
     
     public func didGetReading(_ reading: Float) {
-        // TODO: Implement me
+        self.display.drawText(String(reading), x: 0, y: 0)
     }
 }
