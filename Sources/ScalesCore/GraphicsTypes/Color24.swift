05a153615a815d41d0b66613fe2e79bfb12aba53
David Fearon <david@davidfearon.com>
2023-11-05 17:22:50 +0000

    Make Color24 rgb properties public

--- Sources/ScalesCore/GraphicsTypes/Color24.swift
+++ Sources/ScalesCore/GraphicsTypes/Color24.swift
@@ -3,9 +3,9 @@ import Foundation
 
 public struct Color24 {
     
-    let red: UInt8
-    let green: UInt8
-    let blue: UInt8
+    public let red: UInt8
+    public let green: UInt8
+    public let blue: UInt8
     
     public static var black: Self {
         Self.init(red: 0, green: 0, blue: 0)
