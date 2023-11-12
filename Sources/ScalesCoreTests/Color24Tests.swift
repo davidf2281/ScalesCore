cf78bbf36278e1f73b62546773125d3027c009d3
David Fearon <david@davidfearon.com>
2023-11-12 19:47:48 +0000

    Delete debug print statement

--- Sources/ScalesCoreTests/Color24Tests.swift
+++ Sources/ScalesCoreTests/Color24Tests.swift
@@ -1,9 +1,3 @@
-//
-//  Color24Tests.swift
-//  
-//
-//  Created by David Fearon on 06/11/2023.
-//
 
 import XCTest
 @testable import ScalesCore
@@ -12,7 +6,6 @@ final class Color24Tests: XCTestCase {
 
     func testPacked565() throws {
         let sut = Color24(red: 0b10101010, green: 0b00001111, blue: 0b11110000)
-        print(String(sut.packed565, radix: 2))
         XCTAssertEqual(sut.packed565, 0b1010100001111110)
     }
 }
