276617ef56ab39bd96b88bc1a1af6798587756bd
David Fearon <david@davidfearon.com>
2023-11-15 18:59:10 +0000

    Update screen reolution things

--- Sources/ScalesCore/HardwareAbstractions/Display.swift
+++ Sources/ScalesCore/HardwareAbstractions/Display.swift
@@ -2,5 +2,20 @@
 public protocol Display {
     var width: Int { get }
     var height: Int { get }
+    static var supportedResolutions: [ScreenResolution] { get }
     func showFrame(_ frameBuffer: FrameBuffer)
+}
+
+public enum ScreenResolution {
+    case w320h240
+    case w240h320
+    
+    var size: ScalesCore.Size {
+        switch self {
+            case .w320h240:
+                Size(width: 320, height: 240)
+            case .w240h320:
+                Size(width: 240, height: 320)
+        }
+    }
 }
