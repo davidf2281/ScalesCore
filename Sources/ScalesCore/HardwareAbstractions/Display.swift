ec93ae8db0443f05874e39a1cc204c256a437a5a
David Fearon <david@davidfearon.com>
2023-11-19 12:54:19 +0000

    Remove daft ScreenResolution enum

--- Sources/ScalesCore/HardwareAbstractions/Display.swift
+++ Sources/ScalesCore/HardwareAbstractions/Display.swift
@@ -4,17 +4,3 @@ public protocol Display {
     var height: Int { get }
     func showFrame(_ frameBuffer: FrameBuffer)
 }
-
-public enum ScreenResolution {
-    case w320h240
-    case w240h320
-    
-    var size: ScalesCore.Size {
-        switch self {
-            case .w320h240:
-                Size(width: 320, height: 240)
-            case .w240h320:
-                Size(width: 240, height: 320)
-        }
-    }
-}
