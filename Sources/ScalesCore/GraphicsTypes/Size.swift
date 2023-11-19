633798378a031ee24ff0f6d1b98d5fd4dd175f68
David Fearon <david@davidfearon.com>
2023-11-19 12:56:46 +0000

    Expose width and height property on Size

--- Sources/ScalesCore/GraphicsTypes/Size.swift
+++ Sources/ScalesCore/GraphicsTypes/Size.swift
@@ -1,7 +1,7 @@
 
 public struct Size {
-    let width: Int
-    let height: Int
+    public let width: Int
+    public let height: Int
     
     public init(width: Int, height: Int) {
         self.width = width
