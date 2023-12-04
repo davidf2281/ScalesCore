3aef05dc8cc20cfb5d2f877d310ea94664140ba9
David Fearon <david@davidfearon.com>
2023-12-04 18:05:40 +0000

    Add metadata properties to Sensor protocol

--- Sources/ScalesCore/HardwareAbstractions/Sensor.swift
+++ Sources/ScalesCore/HardwareAbstractions/Sensor.swift
@@ -1,12 +1,43 @@
 
 public protocol Sensor<T>: AnyObject {
     associatedtype T: SensorOutput, Comparable
+    var outputType: SensorOutputType { get }
+    var location: SensorLocation { get }
     var delegate: (any SensorDelegate<T>)? { get set }
     func start()
 }
 
+public enum TemperatureUnit: String {
+    case celsius
+}
+
+public enum PressureUnit: String {
+    case hPa
+}
+
+public enum HumidityUnit: String {
+    case rhd
+}
+
+public enum SensorLocation {
+    case indoor(location: Location?)
+    case outdoor(location: Location?)
+}
+
+public struct Location {
+    let latitude: Double
+    let longitude: Double
+}
+
+public enum SensorOutputType {
+    case temperature(unit: TemperatureUnit)
+    case barometricPressure(unit: PressureUnit)
+    case humidity(unit: HumidityUnit)
+}
+
 public protocol SensorOutput<T>: Sendable, Codable {
-    associatedtype T
+    associatedtype T: Codable
+    
     var stringValue: String { get }
 }
 
