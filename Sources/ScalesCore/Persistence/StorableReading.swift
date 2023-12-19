784e9cca57ba7c92cb06039179c2bb061532d66f
David Fearon <david@davidfearon.com>
2023-12-19 20:12:51 +0000

    Remove unused data property from StorableReading

--- Sources/ScalesCore/Persistence/StorableReading.swift
+++ Sources/ScalesCore/Persistence/StorableReading.swift
@@ -5,14 +5,12 @@ protocol StorableReading<T>: PersistableItem {
     associatedtype T: SensorOutput
     var output: T { get }
     var timestamp: Int { get }
-    var date: Date { get }
 }
 
 struct AnyStorableReading<T: SensorOutput>: StorableReading {
     
     let output: T
     let timestamp: Int
-    let date: Date
     
     var value: Codable {
         self.output
@@ -21,6 +19,5 @@ struct AnyStorableReading<T: SensorOutput>: StorableReading {
     init(value: T, date: Date) {
         self.output = value
         self.timestamp = date.unixMillisSinceEpoch
-        self.date = date
     }
 }
